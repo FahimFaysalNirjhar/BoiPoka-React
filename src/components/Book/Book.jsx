815bf48992c36b21d2aac8e5befd2233c8f57bb0
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-02-16 23:07:28 +0600

    books completed

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "../../App.css";
+import { Star } from "lucide-react";
 
 const Book = ({ book }) => {
   const { bookName, author, image, rating, category, tags } = book;
@@ -6,22 +8,29 @@ const Book = ({ book }) => {
   return (
     <>
       <div>
-        <div className="card bg-base-100 w-96 shadow-sm">
-          <figure>
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt="Shoes"
-            />
+        <div className="card bg-base-100 shadow-sm p-6">
+          <figure className="rounded-2xl bg-[#F3F3F3] py-5">
+            <img src={image} alt="Book" class="w-[134px] h-[166px]" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Card Title</h2>
-            <p>
-              A card component has a figure, a body part, and inside body there
-              are title and actions parts
-            </p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+            <div className="border-b-2 border-dashed border-b-[#13131326] pb-5">
+              {tags.map((tag) => (
+                <button className="btn btn-xs ml-4 rounded-xl text-[#23BE0A] mb-4">
+                  {tag}
+                </button>
+              ))}
+              <h2 className="card-title font-playfair-display text-2xl mb-4">
+                {bookName}
+              </h2>
+              <p className="font-work-sans ">By : {author}</p>
             </div>
+          </div>
+          <div className="flex justify-between">
+            <p>{category}</p>
+            <p className="flex gap-1">
+              {rating}
+              <Star />
+            </p>
           </div>
         </div>
       </div>
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -7,7 +7,7 @@ const Books = ({ data }) => {
       <h1 className="font-playfair-display text-5xl font-bold text-center mb-10">
         Books
       </h1>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {data.map((book) => (
           <Book key={book.bookId} book={book}></Book>
         ))}
